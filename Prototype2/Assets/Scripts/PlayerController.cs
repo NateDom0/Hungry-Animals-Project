cc8f94b88149b74ad226fc3b08a0da81d7ee8d8b
Nathaniel Doromal <nathanielrd98@gmail.com>
2022-05-17 23:56:39 -0700

    Updated animal project
    Added prefabs, and new scripts that allow forward movement for objects.

--- Prototype2/Assets/Scripts/PlayerController.cs
+++ Prototype2/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     //Variables
     public float horizontalInput;
+    public float speed = 20.0f;
+    public float xRange = 15.0f;
+
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //keep the player in bounds
+        if(transform.position.x < -xRange) {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        } else if(transform.position.x > xRange) {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            
+            // Launch a projectile from the player
+            //Instantiate(projectilePrefab); -> launches pizza forward, but doesn't launch at player's position.
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            
+        }
+
     }
 }
