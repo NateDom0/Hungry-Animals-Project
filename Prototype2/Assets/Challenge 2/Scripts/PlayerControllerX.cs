120da543e46a76646982b57b21a5704f2e12dc38
Nathaniel Doromal <nathanielrd98@gmail.com>
2022-05-19 21:23:26 -0700

    Edited p2Mod1

--- Prototype2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Prototype2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,19 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    // use to control time b/w inputs
+    private float timer = 0.0f;
+
     // Update is called once per frame
     void Update()
     {
+        timer -= Time.deltaTime;
+
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && timer <=0)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timer += 1.5f; 
         }
     }
 }
---
+++ Prototype2/Assets/Mod Stuff/ScriptsM/PlayerControllerM.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerM : MonoBehaviour
+{
+    //Variables
+    public float horizontalInput;
+    private float speed = 37.0f;
+    private float xRange = 20.0f;
+
+    public GameObject projectilePrefab; //instantiate new object called 'projectilePrefab' in update()
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //keep the player in bounds
+        if (transform.position.x < -xRange) {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        } else if (transform.position.x > xRange) {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal"); //get users left/right key input
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            
+            // Launch a projectile from the player
+            //Instantiate(projectilePrefab); -> launches pizza forward, but doesn't launch at player's position.
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            
+        }
+
+    }
+}
