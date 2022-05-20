11e672234f25514b17d8ea478fcdedef98cc5000
Nathaniel Doromal <nathanielrd98@gmail.com>
2022-05-19 19:58:04 -0700

    Fixed Challenge Project Basic

--- Prototype2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ Prototype2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class DestroyOutOfBoundsX : MonoBehaviour
 {
-    private float leftLimit = 30;
-    private float bottomLimit = -5;
+    private float leftLimit = -45; // X-axis
+    private float bottomLimit = -1; // Y-axis (affects balls)
 
     // Update is called once per frame
     void Update()
     {
         // Destroy dogs if x position less than left limit
-        if (transform.position.x > leftLimit)
+        // (see bottom comment)
+        if (transform.position.x < leftLimit)
         {
             Destroy(gameObject);
         } 
         // Destroy balls if y position is less than bottomLimit
-        else if (transform.position.z < bottomLimit)
+        else if (transform.position.y < bottomLimit) //z to 'y'
         {
+            //Debug.Log("Game Over");
             Destroy(gameObject);
         }
 
     }
 }
+
+// if x < leftLimit, destroy object (i.e. x=-46)
+// x is going towards the left(negative) limit
--- Prototype2/Assets/Challenge 2/Scripts/MoveForwardX.cs
+++ Prototype2/Assets/Challenge 2/Scripts/MoveForwardX.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveForwardX : MonoBehaviour
 {
-    public float speed;
+    public float speed = 40.0f; // keep it as 'public'
 
     // Update is called once per frame
     void Update()
